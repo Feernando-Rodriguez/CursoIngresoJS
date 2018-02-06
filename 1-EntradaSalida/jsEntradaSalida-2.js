b6e2c6d8613571e071f791d891422b27887be700
Feernando-Rodriguez <36193816+Feernando-Rodriguez@users.noreply.github.com>
2018-02-06 12:15:01 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,14 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var nombre;
+	//esto es un comentario de una linea
+
+	//alert(nombre);
+nombre=prompt("ingrese nombre","Fernando");
+alert("su nombre es: "+nombre);
+/*Hicimos que en el paso anterior nos deje por defecto un nombre pero despues
+nos diga que nuestro nombre es ese*/
+	//var person = prompt("Please enter your name", "Harry Potter");
 }
 
