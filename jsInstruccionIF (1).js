f785d09c73d9d2fc1eaced89bd641ff1caacb9fb
Feernando-Rodriguez <36193816+Feernando-Rodriguez@users.noreply.github.com>
2018-02-07 19:32:03 -0300

    Hice modificaciones en el 1 para mas prolijidad

--- jsInstruccionIF (1).js
+++ jsInstruccionIF (1).js
@@ -2,13 +2,15 @@ function Mostrar()
 {
 //tomo la edad  
 	var edad;
-
 	edad=12;
-    edad=document.getElementById('edad').value;
+	/*Acordarse de asignar alguna edad*/
+ 
+ 	edad=document.getElementById('edad').value;
+
 
 	if(edad==15)
 	{
-	alert("niña bonita");
+		alert("niña bonita");
 	}
 
 
--- jsInstruccionIF (2).js
+++ jsInstruccionIF (2).js
@@ -1,14 +1,7 @@
 function Mostrar()
 {
 //tomo la edad  
-	var edad;
-	edad=12
-	edad=document.getElementById('edad').value;
 
 
-	if(edad>17)
-		{
-		alert("Es mayor de edad");
-		}
 
-	}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓN
