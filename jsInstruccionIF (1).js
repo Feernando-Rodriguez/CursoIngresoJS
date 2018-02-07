9933a13c5e7072e4195a896c4584c43ca2aa2710
Feernando-Rodriguez <36193816+Feernando-Rodriguez@users.noreply.github.com>
2018-02-07 12:17:36 -0300

    Add files via upload
    Hice hasta el trabajo numero 3...

---
+++ jsInstruccionIF (1).js
@@ -0,0 +1,15 @@
+function Mostrar()
+{
+//tomo la edad  
+	var edad;
+
+	edad=12;
+    edad=document.getElementById('edad').value;
+
+	if(edad==15)
+	{
+	alert("niña bonita");
+	}
+
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionIF (2).js
@@ -0,0 +1,14 @@
+function Mostrar()
+{
+//tomo la edad  
+	var edad;
+	edad=12
+	edad=document.getElementById('edad').value;
+
+
+	if(edad>17)
+		{
+		alert("Es mayor de edad");
+		}
+
+	}//FIN DE LA FUNCIÓN
