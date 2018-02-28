e7637fec7b6b036ecf15b42d96ec28ef8d077f5e
Feernando-Rodriguez <36193816+Feernando-Rodriguez@users.noreply.github.com>
2018-02-28 12:09:55 -0300

    Repasados en clase

--- 9-Parciales/parcial2/4-if.js
+++ 9-Parciales/parcial2/4-if.js
@@ -1,7 +1,45 @@
+/*No se tiene que validar y no se usa while... Solamente if. se ingresan dos numeros si son iguales
+se concatenan, si el mayor es el primero se multiplican de lo contario se restan*/
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 4-if");
-	
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno=prompt("Ingrese el primer numero: ");
+
+
+	numeroDos=prompt("Ingrese el segundo numero: ");
+
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	if(numeroUno==numeroDos)
+	{
+		resultado=numeroUno+""+numeroDos;
+	}
+	else
+	{
+		/*numeroUno=parseInt(numeroUno);
+		numeroDos=parseInt(numeroDos);*/
+
+		if(numeroUno>numeroDos)
+		{
+			resultado=numeroUno*numeroDos;
+		}
+		else
+		{
+			resultado=numeroUno-numeroDos;
+		}
+		if(resultado%2==0 && resultado!=0)
+		{
+			resultado=resultado+ " es par.";
+		}
+	}
+	document.write("El resultado es: "+resultado);
+
+
+
 }
 
--- 9-Parciales/parcial2/8-iteraciones.js
+++ 9-Parciales/parcial2/8-iteraciones.js
@@ -1,7 +1,41 @@
+/*Se ingresa hasta que el usuario quiera el nombre de un animal. El peso del mismo
+validar que sea mayor a 0 y la temperatura del habitat de este animal... validar entre -40 y 41°... Informar, 
+la cantidad de temperaturas pares, la cantidad de temperaturas impares, el nombre del animal mas pesado, el nombre del
+animal menos pesado, la cantidad de animales que viven en habitat menores a 0 inclusive, el promedio del peso de todos los animales
+la temperatura maxima y la minima */
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 8-iteraciones");
+	var nombreAnimal;
+	var peso;
+	var temperatura;
+	var respuesta="";
+	var contador=0;
+
+	/*nombreAnimal=prompt("Ingrese un nombre de un animal:");
+	peso=prompt("Ingrese el peso:");
+	temperatura=prompt("Ingrese la temperatura:");*/
+
+	respuesta="si";
+
+	while(respuesta!="no")
+	{
+		contador++;
+
 	
+		nombreAnimal=prompt("Ingrese un nombre de un animal:");
+		peso=prompt("Ingrese el peso:");
+		temperatura=prompt("Ingrese la temperatura:");
+
+		while(peso<0)
+		{
+			peso=prompt("Error, Ingrese el peso mayor a 0");
+		}
+
+		while(temperatura<-40 && temperatura>40)
+		{
+			temperatura=prompt("Error, Ingrese una temperatura entre -40 y 40°")
+		}
+	}
 }
 
