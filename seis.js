da05534aea549293bf90046179d4561be6b37a16
Feernando-Rodriguez <36193816+Feernando-Rodriguez@users.noreply.github.com>
2018-02-27 21:07:54 -0300

    Ahora anda bien...

--- seis.js
+++ seis.js
@@ -1,33 +1,38 @@
-/*Ingrese numeros hasta que el cliente quiera, van a informar cuantos pares e impares ha yen el promedio de los positivos
-ademas de el maximo y el minimo*/
 function Mostrar()
 {
-	var numero;
-	var minimo;
-	var maximo;
-	var seguir="";
-	var negativo=0;
-	var positivo=0;
+	var importe;
 	var contador=0;
-	
-	while(seguir!="no")
+	var menorImporte;
+	var mayorImporte;
+
+	while(contador<24)
 	{
-		numero=prompt("Ingrese su numero");
-		numero=parseInt(numeros);
+		contador++;
 
-		if(numero<0)
+		importe=prompt("Ingrese su importe:");
+
+		while(importe<=0)
 		{
-			negativo+1;
-			alert("El numero es negativo");
-		}
-		else
-		{
-			positivo+1;
-			alert("El numero es positivo");
+			importe=prompt("Ingrese un importe mayor a 0");
 		}
 
+		importe=parseFloat(importe);//Este paso es para que me levante un numero y no asi...
 
+		if(contador==1)
+		{
+			mayorImporte=importe;
+			menorImporte=importe;
+		}
+		else if(importe>mayorImporte)
+		{
+			mayorImporte=importe;
+		}
+		if(importe<menorImporte)
+		{
+			menorImporte=importe;
+		}
 	}
 
-
+	alert("El mayor importe es: "+mayorImporte);
+	alert("El menor importe es: "+menorImporte);
 }
