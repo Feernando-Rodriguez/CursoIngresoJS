faee2be71290c1d84d95a30022cf9f8014f2f607
Feernando-Rodriguez <36193816+Feernando-Rodriguez@users.noreply.github.com>
2018-04-05 12:15:40 -0300

    Primer ejercicio de Funciones
    Primer ejercicio de Funciones

--- WorkSace MASCARAS/main.c
+++ WorkSace MASCARAS/main.c
@@ -1,37 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
-*
-*@param
-*@return
-*/
+//void saludar(void); //firma prototipo
+//int sumar(int numeroUno, int numeroDos);
+float dividirEnteros(int numeroUno,int numeroDos);
+
+
 int main()
+    {
+        int numero;
+        numero=PedirNumeroPar();
+
+        if(sePudo==0)
+        {
+            printf("ok");
+        }
+        else
+        {
+            printf("Mal");
+        }
+    /* float numero;//invocacion
+
+        //numero=sumar(3,3);
+        numero= dividirEnteros(10,3);
+        printf("%f" ,numero);
+
+        return 0;*/
+    }
+/
+
+
+float dividirEnteros(int numeroUno,int numeroDos)
 {
-    char nombre;
-    float sueldo;
-    float sueldoFinal;
-    int aumento;
-    float resultado;
-
-
-    printf("Ingrese su nombre\n");
-    scanf("%c" , &nombre);
-
-    printf("Ingrese su sueldo\n");
-    scanf("%f" , &sueldo);
-
-    printf("Ingrese el aumento\n");
-    scanf("%d" , &aumento);
-
-    sueldoFinal=(sueldo*aumento)/100;
-
-    resultado=sueldoFinal+sueldo;
-
-    printf("Su nombre es %c" ,nombre);
-    printf(" Su sueldo es de: %f" , sueldo );
-    printf(" Su aumento es del: %d" ,aumento);
-    printf(" Su sueldo final es de: %f" , resultado );
-
-    return 0;
+    float resultado=0;
+    resultado=(float)numeroUno/numeroDos;
+    return resultado;
 }
+
+
+/*
+void saludar(void)
+    {
+        printf("Hola mundo!\n");
+    }
+int sumar(int numeroUno,int numeroDos)
+{
+    int resultado=0;
+    resultado=numeroUno+numeroDos;
+    return resultado;
+}
+*/
